7295a2a312fb601875c0fa3d3694f25c4bf990aa
renad-mamdouhh <renadmamdouh110@gmail.com>
2026-05-02 04:21:26 +0300

    renad2
    comments

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
 
-// let rating = 0;
-// let stars = document.querySelectorAll('#stars span');
-
-// stars.forEach(star => {
-//   star.addEventListener('click', () => {
-//     rating = Number(star.getAttribute('data-value'));
-//     updateStars();
-//   });
-// });
-
-// function updateStars() {
-//   stars.forEach(star => {
-//     star.classList.remove('active');
-
-//     if (Number(star.getAttribute('data-value')) <= rating) {
-//       star.classList.add('active');
-//     }
-//   });
-// }
 let rating = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -29,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
       updateStars();
     });
   });
-  displayFeedback();
+//   displayFeedback();
 });
 
 function updateStars() {
@@ -46,103 +27,6 @@ function updateStars() {
 
 
 
-// function validateForm() {
-//     console.log("contact form ");
-//   let n = document.getElementById("name").value.trim();
-//   let e = document.getElementById("email").value.trim();
-//   let m = document.getElementById("message").value.trim();
-
-//   if (n === "" || e === "" || m === "") {
-//     alert("Fill all fields");
-//      alert("Message sent");
-//     return false;
-//   }
-
- 
-  
-
-// let message={
-//     name: n,
-//     email: e,
-//     message: m,
-//     date: new Date().toLocaleString()
-// };
-// let data=JSON.parse(localStorage.getItem("contactMessages")) || [];
-// data.push(message);
-// localStorage.setItem("contactMessages",JSON.stringify(data));
-// alert("message sent");
-// return false;
-// }
-
-
-// function submitFeedback() {
-    
-//   let name = document.getElementById("fname").value.trim();
-//   let msg = document.getElementById("fmsg").value.trim();
-
-//   if (name === "" || msg === "" || rating === 0) {
-//     alert("Please complete all fields and rating");
-//     return false;
-//   }
-
-//   let feedback = {
-//     name,
-//     msg,
-//     rating,
-//     date: new Date().toLocaleDateString()
-//   };
-
-//   let data = JSON.parse(localStorage.getItem('feedbacks')) || [];
-//   data.push(feedback);
-//   localStorage.setItem('feedbacks', JSON.stringify(data));
-
-//   displayFeedback();
-
-//   document.getElementById("fname").value = "";
-//   document.getElementById("fmsg").value = "";
-//   rating = 0;
-//   updateStars();
-//   console.log(name,msg,rating);
-
-//   return false;
-// }
-
-
-// function displayFeedback() {
-//   let list = document.getElementById('feedbacklist');
-//   list.innerHTML = "";
-
-//   let data = JSON.parse(localStorage.getItem('feedbacks')) || [];
-
-//   data.forEach(f => {
-//     let div = document.createElement('div');
-
-//     div.style.background = "#111827";
-//     div.style.padding = "10px";
-//     div.style.marginTop = "10px";
-//     div.style.borderRadius = "10px";
-
-//     div.innerHTML = `
-//       <p><b>${f.name}</b> - ${'★'.repeat(f.rating)}</p>
-//       <p>${f.msg}</p>
-//       <small style="color:gray">${f.date}</small>
-//     `;
-
-//     list.appendChild(div);
-//   });
-// }
-
-// window.onload = displayFeedback;
-
-// // function toggleTheme(){
-// //     let body=document.body;
-// //     if (body.getAttribute("data-theme")=="dark"){
-// //         body.setAttribute("data-theme","light");
-// //     }
-// //     else{
-// //         body.setAttribute("data-theme","dark")
-// //     }
-// // }
 function validateForm() {
   console.log("contact form");
 
@@ -168,7 +52,7 @@ function validateForm() {
 
   alert("Message sent ✅");
 
-  // تنظيف الفورم
+  
   document.getElementById("name").value = "";
   document.getElementById("email").value = "";
   document.getElementById("message").value = "";
@@ -183,7 +67,7 @@ function submitFeedback() {
   let name = document.getElementById("fname").value.trim();
   let msg = document.getElementById("fmsg").value.trim();
 
-  console.log(name, msg, rating); // 👈 للتأكد
+
 
   if (name === "" || msg === "" || rating === 0) {
     alert("Please complete all fields and rating");
@@ -202,7 +86,7 @@ function submitFeedback() {
   localStorage.setItem('feedbacks', JSON.stringify(data));
   alert("message sent");
 
-  displayFeedback();
+//   displayFeedback();
 
   document.getElementById("fname").value = "";
   document.getElementById("fmsg").value = "";
@@ -215,32 +99,32 @@ function submitFeedback() {
 
 
 
-function displayFeedback() {
-  let list = document.getElementById('feedbacklist');
+// function displayFeedback() {
+//   let list = document.getElementById('feedbacklist');
 
-  if (!list) return; 
+//   if (!list) return; 
 
-  list.innerHTML = "";
+//   list.innerHTML = "";
 
-  let data = JSON.parse(localStorage.getItem('feedbacks')) || [];
+//   let data = JSON.parse(localStorage.getItem('feedbacks')) || [];
 
-  data.forEach(f => {
-    let div = document.createElement('div');
+//   data.forEach(f => {
+//     let div = document.createElement('div');
 
-    div.style.background = "#111827";
-    div.style.padding = "10px";
-    div.style.marginTop = "10px";
-    div.style.borderRadius = "10px";
+//     div.style.background = "#111827";
+//     div.style.padding = "10px";
+//     div.style.marginTop = "10px";
+//     div.style.borderRadius = "10px";
 
-    div.innerHTML = `
-      <p><b>${f.name}</b> - ${'★'.repeat(f.rating)}</p>
-      <p>${f.msg}</p>
-      <small style="color:gray">${f.date}</small>
-    `;
+//     div.innerHTML = `
+//       <p><b>${f.name}</b> - ${'★'.repeat(f.rating)}</p>
+//       <p>${f.msg}</p>
+//       <small style="color:gray">${f.date}</small>
+//     `;
 
-    list.appendChild(div);
-  });
-}
+//     list.appendChild(div);
+//   });
+// }
 
 
-window.onload = displayFeedback;
+// window.onload = displayFeedback;
